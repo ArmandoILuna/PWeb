d3b8051b7282598ee33e1f87779f432b0a2ba24a
ArmandoILuna <estudiante.armandoluna@gmail.com>
2023-10-29 19:56:52 -0600

    Se agrego una fucion para el llamado de funciones

--- Fun_Arr.js
+++ Fun_Arr.js
@@ -1,3 +1,5 @@
+const readline = require('readline');
+
 // Función para elevar una base a una potencia
 function power(base, exponent) {
     return Math.pow(base, exponent);
@@ -30,14 +32,55 @@ function calculateAverage(numbers) {
     return sum / numbers.length;
 }
 
-// Llamar a las funciones con los datos ingresados por el usuario
-let resultado1 = power(base, exponent);
-let resultado2 = getLargerInt(number1, number2);
-let resultado3 = fibonacciSequence(limit);
-let resultado4 = calculateAverage(numbers);
+// Función para preguntar al usuario qué función desea ejecutar
+function askUser() {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
 
-// Imprimir los resultados
-console.log(`El resultado de ${base} elevado a la potencia de ${exponent} es ${resultado1}`);
-console.log(`El número mayor entre ${number1} y ${number2} es ${resultado2}`);
-console.log(`Los primeros ${limit} números de la secuencia de Fibonacci son: ${resultado3}`);
-console.log(`El promedio de los números ${numbers} es ${resultado4}`);
+    rl.question("¿Qué función desea ejecutar? (power, getLargerInt, fibonacciSequence, calculateAverage)", function(selectedFunction) {
+        switch (selectedFunction) {
+            case "power":
+                rl.question("Ingrese la base:", function(base) {
+                    rl.question("Ingrese el exponente:", function(exponent) {
+                        let resultado1 = power(parseInt(base), parseInt(exponent));
+                        console.log(`El resultado de ${base} elevado a la potencia de ${exponent} es ${resultado1}`);
+                        rl.close();
+                    });
+                });
+                break;
+            case "getLargerInt":
+                rl.question("Ingrese el primer número:", function(number1) {
+                    rl.question("Ingrese el segundo número:", function(number2) {
+                        let resultado2 = getLargerInt(parseInt(number1), parseInt(number2));
+                        console.log(`El número mayor entre ${number1} y ${number2} es ${resultado2}`);
+                        rl.close();
+                    });
+                });
+                break;
+            case "fibonacciSequence":
+                rl.question("Ingrese el límite de la secuencia de Fibonacci:", function(limit) {
+                    let resultado3 = fibonacciSequence(parseInt(limit));
+                    console.log(`Los primeros ${limit} números de la secuencia de Fibonacci son: ${resultado3}`);
+                    rl.close();
+                });
+                break;
+            case "calculateAverage":
+                rl.question("Ingrese los números separados por comas:", function(numbers) {
+                    let resultado4 = calculateAverage(numbers.split(",").map(Number));
+                    console.log(`El promedio de los números ${numbers} es ${resultado4}`);
+                    rl.close();
+                });
+                break;
+            default:
+                console.log("Función no válida.");
+                rl.close();
+                break;
+        }
+    });
+}
+
+// Llamar a la función para preguntar al usuario qué función desea ejecutar
+askUser();
+
